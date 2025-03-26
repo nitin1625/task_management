cb13c14c344674f47ade4bddf2bf76f2f89c23f2
nitinsharma <nitin.sharma@sonartechnologies.com>
2025-03-26 11:39:40 +0530

    .env addition

--- task_management/settings.py
+++ task_management/settings.py
@@ -1,15 +1,17 @@
 import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-abc123!change-this-in-production!'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -125,5 +127,5 @@ CORS_ALLOW_ALL_ORIGINS = DEBUG  # Set to False in production
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    # Add production domains here
+    
 ]
