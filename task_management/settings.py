1b1592f764efd38d27bb6624a9b2f9506451b574
nitinsharma <nitin.sharma@sonartechnologies.com>
2025-03-26 13:23:10 +0530

    settings.py

--- task_management/settings.py
+++ task_management/settings.py
@@ -8,10 +8,10 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v%$wdio@)!)$r1eh3k&ite4f^qp%z#(9m6&b$zoa)#v#&d0&6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
